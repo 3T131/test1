430fb5dfee2fc8463f003546602446532b5ba003
3T131 <1240996799@qq.com>
2018-02-24 15:26:58 +0800

    第一次上传

--- src/test/java/Shangji1.java
+++ src/test/java/Shangji1.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Shangji1 {
     public static void main(String[] args) {
         Session session= HibernateUtil.openSession();
-
+//加注释
         /*
         // 查询所有列表
 
